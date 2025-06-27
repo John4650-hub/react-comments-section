7aa59649086e70bf1acebc35eba36b046244bf19
john4650-hub <johndelvin51@gmail.com>
2025-06-27 17:45:10 +0300

    removed old package*.json files

--- src/components/CommentStructure.tsx/DeleteModal.tsx
+++ src/components/CommentStructure.tsx/DeleteModal.tsx
@@ -1,6 +1,5 @@
-import 'react-responsive-modal/styles.css'
-import { Modal } from 'react-responsive-modal'
 import { GlobalContext } from '../../context/Provider'
+import Modal from 'react-modal';
 
 interface DeleteModalProps {
   comId: string
@@ -13,14 +12,33 @@ const DeleteModal = ({ comId, parentId }: DeleteModalProps) => {
   const onCloseModal = () => setOpen(false)
   const globalStore: any = React.useContext(GlobalContext)
 
+  const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    maxWidth: '240px',
+    margin:'auto',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '2rem',
+    borderRadius: '8px'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  }
+};
+
+
   return (
     <div>
       <div style={{ width: '100%' }} onClick={onOpenModal}>
         delete
       </div>
-      <Modal open={open} onClose={onCloseModal} center>
-        <h2>Are you sure?</h2>
-        <p>Once you delete this comment it will be gone forever.</p>
+      <Modal isOpen={open} onAfterOpen={onOpenModal} onRequestClose={onCloseModal} style={customStyles}>
+        <h2 style={{textAlign: 'center'}}>Are you sure?</h2>
+        <p style={{textAlign: 'center'}}>Once you delete this comment it will be gone forever.</p>
         <div className='deleteBtns'>
           <button
             className='delete'
